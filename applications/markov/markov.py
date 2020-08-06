d85398a5fe0a5b0775eb44352f8ba41d79d190ad
Jess Daniel <jessdaniel474@gmail.com>
2020-08-06 17:06:53 -0400

    lookup_table and expensive_seq

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,69 @@
 import random
 
+# Understand:
+
+# Plan:
+# Read input.txt and spit into words
+# Analyze the text, building up the dataset
+# choose a random "start word" to begin
+# start word starts with " or first or second starts with capital letter
+# Make a list of start words
+# loop through, choose a random following word
+# stop at a stop word
+# word ends with a . ? ! ... or second to last character
+
+# why a hashtable? Good way to relate info, associate key/value pair
+# frequent lookups, key: word, value: list of words that can follow that word
+
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+    # print(words) -> prints whole text
+    # split into words
+split_words = words.split()
+
 
 # TODO: analyze which words can follow other words
 # Your code here
+dataset = {}
+
+for i in range(len(split_words) - 1):
+    word = split_words[i]
+    next_word = split_words[i + 1]
+
+    # if word is not in the dataset, add it in
+    if word not in dataset:
+        dataset[word] = [next_word]
+    # word is in dataset, so add to the list
+    else:
+        dataset[word].append(next_word)
+
+# Make a list of start words
+start_words = []
+for key in dataset.keys():
+    if key[0].isupper() or len(key) > 1 and key[1].isupper():
+        start_words.append(key)
+
+word = random.choice(start_words)
 
 
 # TODO: construct 5 random sentences
 # Your code here
+# TODO: wrap around another loop to get 5 random sentences
 
+stopped = False
+stop_signs = "?.!"
+count = 0
+
+while count < 5:
+    count += 1
+    stopped = False
+    print(count)
+    while not stopped:
+        print(word, end=" ")
+        # if stop word, stop
+        if word[-1] in stop_signs or len(word) > 1 and word[-2] in stop_signs:
+            stopped = True
+        # choose a randon following word
+        following_words = dataset[word]
+        word = random.choice(following_words)
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,25 @@
+cache = {}
+
+chars_to_ignore = ":;,.-+=/'"
+
+
 def word_count(s):
     # Your code here
+    # returns a words with their counts
+    words = s.split()
+    print(words)
+    for character in chars_to_ignore:
+        # error: list has no object replace
+        new_words = words.replace(character, "")
+        return new_words
+    print(new_words)
 
 
+word_count("Hello, my cat. And my cat doesn't say hello back.")
 
-if __name__ == "__main__":
-    print(word_count(""))
-    print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+# if __name__ == "__main__":
+#     print(word_count(""))
+#     print(word_count("Hello"))
+#     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+#     print(word_count(
+#         'This is a test of the emergency broadcast network. This is only a test.'))
