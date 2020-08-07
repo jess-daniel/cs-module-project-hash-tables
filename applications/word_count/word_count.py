e4a831deb40cba5940e155c35f238c2958c82c7b
Jess Daniel <jessdaniel474@gmail.com>
2020-08-06 21:12:55 -0400

    word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,25 +1,25 @@
 cache = {}
 
-chars_to_ignore = ":;,.-+=/'"
+chars_to_ignore = " \" : ; , . - + = / \ | [ ] { } ( ) * ^ &"
 
 
 def word_count(s):
     # Your code here
     # returns a words with their counts
-    words = s.split()
-    print(words)
-    for character in chars_to_ignore:
-        # error: list has no object replace
-        new_words = words.replace(character, "")
-        return new_words
-    print(new_words)
+    words = s.lower().split()
+    # print(words)
+
+    # strips the chars out while making a new array
+    new_words = [word.strip(chars_to_ignore) for word in words]
+
+    for word in new_words:
+        cache[word] = new_words.count(word)
+    return cache
 
 
-word_count("Hello, my cat. And my cat doesn't say hello back.")
-
-# if __name__ == "__main__":
-#     print(word_count(""))
-#     print(word_count("Hello"))
-#     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-#     print(word_count(
-#         'This is a test of the emergency broadcast network. This is only a test.'))
+if __name__ == "__main__":
+    print(word_count(""))
+    print(word_count("Hello"))
+    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -94,6 +94,7 @@ class HashTable:
         # Your code here
         hashed_idx = self.hash_index(key)
 
+        # nothing at index
         if not self.new_list[hashed_idx]:
             self.new_list[hashed_idx] = HashTableEntry(key, value)
             self.size += 1
@@ -130,6 +131,7 @@ class HashTable:
 
         if not current:
             print(f"Error: There is no key {key}")
+        # only one item so delete it
         elif not current.next:
             self.new_list[hashed_idx] = None
             self.size -= 1
